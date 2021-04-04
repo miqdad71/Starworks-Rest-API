16a13ace03b4551a6edf89139d72d416db4f5f41
miqdad71 <miqdad.ihbs71777@gmail.com>
2021-04-04 16:41:21 +0700

    get engineer not autorization

--- src/routes/engineer.js
+++ src/routes/engineer.js
@@ -14,7 +14,7 @@ const {
 
 const uploadImage = require('../middleware/multer')
 
-router.get('/', authorization, getAllEngineer)
+router.get('/', getAllEngineer)
 router.get('/detail/:acId', authorization, getEngineerById)
 router.get('/filter', authorization, getFilterEngineer)
 router.put('/:enId', authorization, uploadImage, updateEngineer)
